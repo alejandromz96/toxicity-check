44fde298d24dd31a6c2cce6fc2975d7776061dc3
cpl121 <cpldev121@gmail.com>
2024-02-17 16:26:50 +0100

    fix: format

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,17 +1,17 @@
 import styles from '../styles/Loader.module.css'
 
 const Loader = (): JSX.Element => {
-  return (
-      <div className={styles.blobs}>
-        <div className={styles.blob_center}></div>
-        <div className={styles.blob}></div>
-        <div className={styles.blob}></div>
-        <div className={styles.blob}></div>
-        <div className={styles.blob}></div>
-        <div className={styles.blob}></div>
-        <div className={styles.blob}></div>
-      </div>
-  );
-};
+    return (
+        <div className={styles.blobs}>
+            <div className={styles.blob_center}></div>
+            <div className={styles.blob}></div>
+            <div className={styles.blob}></div>
+            <div className={styles.blob}></div>
+            <div className={styles.blob}></div>
+            <div className={styles.blob}></div>
+            <div className={styles.blob}></div>
+        </div>
+    )
+}
 
-export default Loader;
+export default Loader
