4e9c451c80e9fe77f14f73aeab7537a86ef8d49b
cpl121 <cpldev121@gmail.com>
2024-03-02 18:21:26 +0100

    feat: minor improvements

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,12 +1,13 @@
 import type { JSX } from 'react'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
-import { UserCircleIcon } from '@heroicons/react/24/solid'
+import { UserCircleIcon, AtSymbolIcon, ComputerDesktopIcon, CommandLineIcon } from '@heroicons/react/24/solid'
 
 interface IMediaUrls {
     linkedIn?: string
     github?: string
     portfolio?: string
+    twitter?: string
 }
 
 interface IPromoCard {
@@ -23,11 +24,12 @@ const PERSONAL_DATA: IPromoCard[] = [
     {
         name: 'CPL121',
         profileImage: 'https://raw.seadn.io/files/576dd82b5edd4bf7ebcba49251e28175.jpg',
-        description: 'Fullstack web3 Developer',
+        description: 'Fullstack Web3 Developer',
         links: {
             linkedIn: 'https://es.linkedin.com/in/c%C3%A9sar-pe%C3%B3n-lamparero/',
             github: 'https://github.com/cpl121/',
             portfolio: 'https://cpl121.eth.limo/',
+            twitter: 'https://twitter.com/CPL121_',
         },
     },
     {
@@ -50,19 +52,18 @@ export default function AboutPage(): JSX.Element {
         await router.push('/')
     }
     return (
-        <div className="flex flex-col justify-around h-screen">
-            <span className="text-2xl">ABOUT PAGE</span>
+        <div className="flex flex-col justify-around h-fit w-full space-y-4 max-w-96">
+            <span className="text-2xl text-center">ABOUT PAGE</span>
             <div className="flex flex-col space-y-8">
                 {PERSONAL_DATA.map((data, index) => (
-                    <div className="flex flex-col border-2 border-solid rounded-xl p-4 w-96 h-fit space-y-2 hover:shadow-lg hover:shadow-emerald-500" key={index}>
+                    <div className="flex flex-col border-2 border-solid rounded-xl p-4 w-full h-fit space-y-4 hover:shadow-lg hover:shadow-emerald-500" key={index}>
                         <span className="text-bold text-center text-xl border-b border-solid">{data.name}</span>
-                        <div className="flex flex-row space-x-4">
+                        <div className="flex flex-row space-x-6">
                             {data.profileImage ? (
                                 <img
                                     src={data.profileImage}
                                     alt={data.name}
-                                    width={100}
-                                    height={66}
+                                    width={'40%'}
                                     style={{ borderRadius: '100%' }}
                                 />
                             ) : (
@@ -74,35 +75,43 @@ export default function AboutPage(): JSX.Element {
                                     style={{ borderRadius: '100%' }}
                                 />
                             )}
-                            <div className="flex flex-col space-y-4">
+                            <div className="flex flex-col space-y-3 items-start">
                                 {data.links.portfolio && (
-                                    <div className="flex flex-row space-x-2 justify-center items-center">
-                                        <UserCircleIcon className="w-5 h-5" />
-                                        <a href={data.links.portfolio} target="_blank" rel="noopener noreferrer">
+                                    <div className="flex flex-row space-x-2 justify-center items-center hover:text-cyan-400">
+                                        <ComputerDesktopIcon className="w-5 h-5" />
+                                        <a href={data.links.portfolio} target="_blank" rel="noopener noreferrer" className='link'>
                                             Portfolio
                                         </a>
                                     </div>
                                 )}
                                 {data.links.github && (
-                                    <div className="flex flex-row space-x-2 justify-center items-center">
-                                        <UserCircleIcon className="w-5 h-5" />
-                                        <a href={data.links.github} target="_blank" rel="noopener noreferrer">
+                                    <div className="flex flex-row space-x-2 justify-center items-center hover:text-cyan-400">
+                                        <CommandLineIcon className="w-5 h-5" />
+                                        <a href={data.links.github} target="_blank" rel="noopener noreferrer" className='link'>
                                             Github
                                         </a>
                                     </div>
                                 )}
                                 {data.links.linkedIn && (
-                                    <div className="flex flex-row space-x-2 justify-center items-center">
+                                    <div className="flex flex-row space-x-2 justify-center items-center hover:text-cyan-400">
                                         <UserCircleIcon className="w-5 h-5" />
-                                        <a href={data.links.linkedIn} target="_blank" rel="noopener noreferrer">
+                                        <a href={data.links.linkedIn} target="_blank" rel="noopener noreferrer" className='link'>
                                             LinkedIn
+                                        </a>
+                                    </div>
+                                )}
+                                {data.links.twitter && (
+                                    <div className="flex flex-row space-x-2 justify-center items-center hover:text-cyan-400">
+                                        <AtSymbolIcon className="w-5 h-5" />
+                                        <a href={data.links.twitter} target="_blank" rel="noopener noreferrer" className='link'>
+                                            Twitter
                                         </a>
                                     </div>
                                 )}
                             </div>
                         </div>
                         {data.description && (
-                            <span className='text-center text-lg pt-4'>{data.description}</span>
+                            <span className='text-center text-lg'>{data.description}</span>
                         )}
                     </div>
                 ))}
