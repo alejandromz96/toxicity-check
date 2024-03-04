92f53f2dbd74fdbf0042bd0102e6cb8fb73ddec1
cpl121 <cpldev121@gmail.com>
2024-03-04 20:04:56 +0100

    fix: constants improvements

--- src/lib/constants/url.constants.ts
+++ src/lib/constants/url.constants.ts
@@ -1,5 +1,5 @@
 const PRODUCTION_URL = 'https://toxicitycheck.vercel.app'
-const X_URL = 'https://twitter.com/'
+const X_URL = 'https://x.com/'
 const X_TEXT_FOR_URL = (score: number): string =>
-    `I just got a score of ${score} on this amazing app! Visit ${PRODUCTION_URL} to try it yourself.`
+    `I just got a ${score} toxicity score! Do you think you can be worse than me? Go and check how toxic can you be! ${PRODUCTION_URL}`
 export const X_URL_TO_SHARE_SCORE = (score: number): string => `${X_URL}intent/tweet?text=${X_TEXT_FOR_URL(score)}`
