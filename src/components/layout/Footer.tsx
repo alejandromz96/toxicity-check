9ca8d5e426936f47d1ea02f49cb930041c5ca742
alejandromz96 <37580269+alejandromz96@users.noreply.github.com>
2024-03-07 22:20:52 +0100

    chore: colors

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 const Footer: React.FC = () => (
-    <footer className="bg-gradient-to-b from-indigo-700 to-indigo-950 to-30% p-1 fixed w-[100%] bottom-0 flex justify-center items-center z-20">
+    <footer className="bg-gradient-to-b from-indigo-800 to-indigo-950 to-15% p-1 fixed w-[100%] bottom-0 flex justify-center items-center z-20">
         <span className="mr-4 text-sm font-bold text-neutral-300 hover:text-neutral-400">
             {' '}
             <Link href="https://github.com/alejandromz96/toxicity-check" target="_blank">
